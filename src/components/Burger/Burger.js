c648bf69377c3961e07b39d1fffa4a12ac7d0e1c
Peter Eck <jpeckiii@gmail.com>
2018-09-11 18:07:46 -0500

    Add state for dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = ( props ) => {
+    // Dynamically rendering ingredients
+    let newIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+            return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        });
+    }).reduce((acc, next) => {
+        return acc.concat(next);
+    }, []);
+
+    if (newIngredients.length === 0) {
+        newIngredients = <p>Please start adding ingredients!</p>
+    }
+    console.log(newIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {newIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
