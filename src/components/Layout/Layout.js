a21791665bf0a823ec40b017b576526f20816683
Peter Eck <jpeckiii@gmail.com>
2018-09-27 18:41:26 -0500

    Finish SideDrawer and more responsiveness

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../HOC/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.css"
 
-const Layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state= {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !this.state.showSideDrawer};
+        });
+    }
+
+    render () {
+       return (
+        <Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+            <SideDrawer 
+                opened={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}
+             />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>  
+       )
+   }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,27 @@ import React from 'react'
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../HOC/Aux";
 
 const SideDrawer = ( props ) => {
-    
+    let attachedClasses = [classes.SideDrawer, classes.Closed];
+    if (props.opened) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}> 
-                <Logo />
+        <Aux>
+            <Backdrop show={props.opened} clicked={props.closed} />
+            <div className={attachedClasses.join(" ")}>
+                <div className={classes.Logo}> 
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 }
 
