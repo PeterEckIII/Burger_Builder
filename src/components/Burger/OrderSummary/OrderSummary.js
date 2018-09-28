6080fa62869cb71c7080aa7008aa89cd4e92199b
Peter Eck <jpeckiii@gmail.com>
2018-09-27 19:06:35 -0500

    Fix performance issues and change folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import Aux from "../../../HOC/Aux";
+import React, { Component } from 'react';
+import Aux from "../../../HOC/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
 
-const OrderSummary = ( props ) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>
-                {igKey}
-            </span>: {props.ingredients[igKey]}
-        </li>
+class OrderSummary extends Component {
+    
+    // This could be a functional component, just keeping this here for reference and debugging
+    componentWillUpdate() {
+        console.log("Order Summary will update");
+    }
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>
+                    {igKey}
+                </span>: {this.props.ingredients[igKey]}
+            </li>
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger, made your way!</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger, made your way!</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
 }
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import Aux from "../../../HOC/Aux";
+import React, { Component } from 'react';
+import Aux from "../../../HOC/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.css";
 
-const Modal = ( props ) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
+
+    // Added the below methods because the Modal wraps the OrderSummary component, which was rendering unnecessarily, causing performance issues
+    shouldComponentUpdate( nextProps, nextState ) {
+        if (nextProps.show !== this.props.show) {
+            return true;
+        }
+    }
+
+    componentWillUpdate() {
+        console.log("[Modal] WillUpdate");
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
 export default Modal;
