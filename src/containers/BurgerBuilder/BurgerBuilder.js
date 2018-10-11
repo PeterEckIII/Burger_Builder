af7a64f847ca03f21c2ee16979f55e30ba2a708c
Peter Eck <jpeckiii@gmail.com>
2018-10-11 16:55:53 -0500

    Finish burger builder actions

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState (ingredients) {
@@ -57,7 +57,7 @@ class BurgerBuilder extends Component {
             orderSummary = <Spinner />
         }
 
-        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
+        let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
 
         if (this.props.ings) {
             burger = (
@@ -94,14 +94,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: ( ingName ) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: ( ingName ) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+        onIngredientRemoved: ( ingName ) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
     }
 }
 
